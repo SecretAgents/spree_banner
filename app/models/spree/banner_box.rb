18fc837366e86268c2c85e993cc2aaa969e061f3
Igor Narazin <in@isfb.ru>
2014-08-19 17:48:19 +0400

    remove paperclip spree settings

--- app/models/spree/banner_box.rb
+++ app/models/spree/banner_box.rb
@@ -28,10 +28,6 @@ module Spree
       end
     }
 
-    # Load user defined paperclip settings
-    include Spree::Core::S3Support
-    supports_s3 :attachment
-
     Spree::BannerBox.attachment_definitions[:attachment][:styles] = ActiveSupport::JSON.decode(SpreeBanner::Config[:banner_styles]).symbolize_keys!
     Spree::BannerBox.attachment_definitions[:attachment][:path] = SpreeBanner::Config[:banner_path]
     Spree::BannerBox.attachment_definitions[:attachment][:url] = SpreeBanner::Config[:banner_url]
